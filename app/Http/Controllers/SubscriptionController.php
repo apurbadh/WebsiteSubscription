19f541b6826ae5e2c73c82bc386cefde824ef8e0
Apurba Adhikari <erikduke182@gmail.com>
2022-05-27 14:59:53 +0545

    Subscription errors fixed

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Subscription;
 use App\Models\Website;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
@@ -11,19 +12,28 @@ class SubscriptionController extends Controller
 
     public function subscribe(Request $request, $website)
     {
-        $data = $request->validate(Subscription::$rules);
+        $data = $request->input();
+        $validator = Validator::make($data, Subscription::$rules);
+
+        if ($validator->fails())
+        {
+            return [
+                'sucess' => false,
+                'message' => "Invalid Email"
+            ];
+        }
         $website = Website::where('website_name', $website)->first();
 
         if ($website == null)
         {
-            abort(404);
+            return ['sucess' => false, 'message' => "Website Not Found !"];
         }
 
         $subscription = Subscription::where('email', $data['email'])->first();
 
         if (!$subscription == null && $subscription->website->website_name == $website)
         {
-            return ['message' => "Already Subscribed !", 'type' => "failed"];
+            return ['message' => "Already Subscribed !", 'sucess' => false];
         }
 
         $toInsert = [
@@ -33,6 +43,6 @@ class SubscriptionController extends Controller
 
         Subscription::create($toInsert);
 
-        return ['message' => "Subscribed to website sucessfully !", 'type' => "sucess"];
+        return ['message' => "Subscribed to website sucessfully !", 'sucess' => true];
     }
 }
